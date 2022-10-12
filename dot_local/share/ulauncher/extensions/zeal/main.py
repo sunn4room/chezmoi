90f55c25397ee3e8df0330ca5bec0d7f6c8dbeaa
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-12 02:37:40 +0000

    Adding tarfile member sanitization to extractall()

--- dot_local/share/ulauncher/extensions/zeal/main.py
+++ dot_local/share/ulauncher/extensions/zeal/main.py
@@ -147,7 +147,26 @@ class MainExtension(AdvancedExtension):
                 target_dir = os.path.join(self.docsets_path, target_name)
                 if os.path.isdir(target_dir):
                     shutil.rmtree(target_dir)
-                f.extractall(self.docsets_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, self.docsets_path)
             os.system('pkill zeal')
 
 if __name__ == '__main__':
